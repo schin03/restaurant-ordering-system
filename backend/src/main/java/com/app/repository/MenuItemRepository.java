66be8788f610ded802ac029e8b3ae405378a95ba
Sam Chin <thomasamc7@gmail.com>
2026-07-07 15:50:30 -0700

    Successfully seeded menu items to db

--- backend/src/main/java/com/app/repository/MenuItemRepository.java
+++ backend/src/main/java/com/app/repository/MenuItemRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.model.MenuItem;
 
-
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long>{
     
 }
